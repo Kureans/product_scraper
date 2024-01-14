0e184ef7d6ccc76742e5952b325b7a7cb3063831
leonardtengys <leonardtengys@gmail.com>
2024-01-14 20:39:18 +0800

    Add db insertion logic along with iso timezone

--- scraper.py
+++ scraper.py
@@ -13,7 +13,12 @@ from db import get_db_connection
 
 import statistics
 import os
+import traceback
+from datetime import timezone
+import datetime
 
+
+TIMEOUT_SECONDS = 5
 BASE_URL_AMAZON = 'https://www.amazon.sg/s?k='
 DELIMITER_AMAZON = '+'
 
@@ -37,12 +42,23 @@ class PriceStats():
                     Lowest Price: {self.lowest}, \
                     Median Price: {self.median}, \
                     Mean Price: {self.mean}'
-        
+    
+
+def price_stats_to_prices_row_entry(stat: PriceStats):
+    return {
+        'query_id': stat.query_id,
+        'query_dt': datetime.datetime.now(timezone.utc).isoformat(),
+        'median_price': stat.median,
+        'lowest_price': stat.lowest,
+        'highest_price': stat.highest,
+        'mean_price': stat.mean
+    }
+
 def init_driver() -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
     driver = webdriver.Chrome(options=chrome_options, service=ChromeService(ChromeDriverManager().install()))
-    driver.set_page_load_timeout(10)
+    driver.set_page_load_timeout(TIMEOUT_SECONDS)
     return driver
 
 def get_full_query_url(base_url, delimiter, query_terms):
@@ -56,7 +72,7 @@ def scrape_lazada(driver: webdriver.Chrome, context: QueryContext, client: Clien
     full_query_url = get_full_query_url(BASE_URL_LAZADA, DELIMITER_LAZADA, context.query_terms)
     try:
         driver.get(full_query_url)
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, TIMEOUT_SECONDS).until(
             EC.presence_of_element_located((By.ID, 'products'))
         )
         
@@ -96,7 +112,7 @@ def scrape_amazon(driver: webdriver.Chrome, context: QueryContext, client: Clien
     full_query_url = get_full_query_url(BASE_URL_AMAZON, DELIMITER_AMAZON, context.query_terms)
     try:
         driver.get(full_query_url)
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, TIMEOUT_SECONDS).until(
             EC.presence_of_element_located((By.ID, 's-skipLinkTargetForMainSearchResults'))
         )
 
@@ -109,6 +125,8 @@ def scrape_amazon(driver: webdriver.Chrome, context: QueryContext, client: Clien
             print("Empty List!")
         else:
             prices = []
+            if len(context.exclude_terms) > 0:
+                elements = filter(lambda e: e not in context.exclude_terms, elements)
             for e in elements:
                 price_data = e.text.split('S')
                 if len(price_data) < 2:
@@ -131,9 +149,18 @@ def scrape_amazon(driver: webdriver.Chrome, context: QueryContext, client: Clien
             print(stats)
             
             return stats
-            
+
+def add_stats_to_db(client: Client, stats_list: list[dict]):
+    try:
+        data, count = client.table('prices').insert(stats_list).execute()
+        print(data)
+        print(count)
+    except Exception:
+        print(traceback.format_exc())
+
 if __name__ == '__main__':
     try:
+        print(datetime.datetime.now(timezone.utc).isoformat())
         client = get_db_connection(url=SUPABASE_URL, key=SUPABASE_KEY)
         context_list = get_query_context_list(client)
         driver: webdriver.Chrome = init_driver()
@@ -141,6 +168,10 @@ if __name__ == '__main__':
         for ctx in context_list:
             stats_list.append(scrape_lazada(driver, ctx, client))
             stats_list.append(scrape_amazon(driver, ctx, client))
-        print(stats_list)
+
+        stats_list = list(map(lambda stat : price_stats_to_prices_row_entry(stat), stats_list))
+        for s in stats_list:
+            print(s)
+        add_stats_to_db(client, stats_list)
     finally:
         driver.quit()
