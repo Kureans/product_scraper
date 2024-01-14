dc799dd50f5b245fbbc62c70e12eb21368f9fe8c
leonardtengys <leonardtengys@gmail.com>
2024-01-14 17:50:22 +0800

    Implement scraping logic for lazada

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.wait import WebDriverWait
@@ -11,24 +10,66 @@ import statistics
 BASE_URL_AMAZON = 'https://www.amazon.sg/s?k='
 DELIMITER_AMAZON = '+'
 
-URL_LAZADA = 'https://www.lazada.sg/catalog/?q=whey%20protein%202.5kg%20myprotein'
+BASE_URL_LAZADA = 'https://www.lazada.sg/catalog/?q='
+DELIMITER_LAZADA = '%20'
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('headless')
+chrome_options.add_argument('headless')
 driver = webdriver.Chrome(options=chrome_options, service=ChromeService(ChromeDriverManager().install()))
+driver.set_page_load_timeout(10)
 query_terms = ['whey', 'protein', '2.5kg']
 
-def scrape_amazon(driver, query_terms):
+def get_full_query_url(base_url, delimiter, query_terms):
     query_params = ''
     for term in query_terms:
-        query_params += f'{term}{DELIMITER_AMAZON}'
-    query_params = query_params[:-1]
-    full_query_url = BASE_URL_AMAZON + query_params
-    driver.get(full_query_url)
+        query_params += f'{term}{delimiter}'
+    query_params = query_params[:-(len(delimiter))]
+    return base_url + query_params
+
+def scrape_lazada(driver: webdriver.Chrome, query_terms: list):
+    full_query_url = get_full_query_url(BASE_URL_LAZADA, DELIMITER_LAZADA, query_terms)
     try:
+        driver.get(full_query_url)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'products'))
+        )
+        
+    except TimeoutException:
+        print("Timeout Occured when scraping Lazada!")
+
+    finally:
+        elements = driver.find_elements(By.CSS_SELECTOR, f'[data-qa-locator="product-item"]')
+        if len(elements) == 0:
+            print("Empty List!")
+        else:
+            prices = []
+            for e in elements:
+                product_data = e.text.split('\n')
+                for item in product_data:
+                    if item[0] != '$':
+                        continue
+                    price_value = float(item[1:])
+                    prices.append(price_value)
+
+            print('Lazada: ')
+            print(f'Highest Price: {max(prices)}, \
+                    Lowest Price: {min(prices)}, \
+                    Median Price: {statistics.median(prices)}, \
+                    Mean Price: {statistics.mean(prices)}')
+                
+
+def scrape_amazon(driver: webdriver.Chrome, query_terms: list):
+    full_query_url = get_full_query_url(BASE_URL_AMAZON, DELIMITER_AMAZON, query_terms)
+    try:
+        driver.get(full_query_url)
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, 's-skipLinkTargetForMainSearchResults'))
         )
+
+    except TimeoutException:
+        print("Timeout Occured when scraping Amazon!")
+          
+    finally:
         elements = driver.find_elements(By.CSS_SELECTOR, f'[data-cy="price-recipe"]')
         if len(elements) == 0:
             print("Empty List!")
@@ -42,13 +83,15 @@ def scrape_amazon(driver, query_terms):
                 dollars, cents, *metadata = price_string.split('\n', 2)
                 price_value = int(dollars[1:]) + (int(cents[:2]) / 100)
                 prices.append(price_value)
+            
+            print('Amazon: ')
             print(f'Highest Price: {max(prices)}, \
                     Lowest Price: {min(prices)}, \
                     Median Price: {statistics.median(prices)}, \
                     Mean Price: {statistics.mean(prices)}')
-    except TimeoutException:
-        print("Timeout Occured")
-    finally:
-        driver.quit()
 
-scrape_amazon(driver, query_terms)
+try:
+    scrape_lazada(driver, query_terms)
+    scrape_amazon(driver, query_terms)
+finally:
+    driver.quit()
